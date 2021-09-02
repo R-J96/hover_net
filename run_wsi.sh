db13692cb86afd9f1e8be372eb4d752c0b29c93f
R-J96 <robjewsbury@gmail.com>
2021-09-02 13:48:30 +0100

    WIP: changing for UHCW slides

--- run_wsi.sh
+++ run_wsi.sh
@@ -2,14 +2,14 @@ python run_infer.py \
 --gpu='0,1' \
 --nr_types=6 \
 --type_info_path=type_info.json \
---batch_size=64 \
+--batch_size=32 \
 --model_mode=fast \
---model_path=../pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--model_path=models/pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 wsi \
---input_dir=dataset/sample_wsis/wsi/ \
---output_dir=dataset/sample_wsis/out/ \
---input_mask_dir=dataset/sample_wsis/msk/ \
+--input_dir=/data/UHCW/WSIs/Test/ \
+--output_dir=/data/Test/hoverNetUHCWTest/ \
+--input_mask_dir= \
 --save_thumb \
 --save_mask
