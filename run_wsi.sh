516afa3bd90d704437ce8b3a7674345b610a34d3
R-J96 <robjewsbury@gmail.com>
2021-09-04 09:31:59 +0100

    WIP: lowerGU panNuke model inference

--- run_wsi.sh
+++ run_wsi.sh
@@ -1,15 +1,15 @@
 python run_infer.py \
 --gpu='0,1' \
 --nr_types=6 \
---type_info_path=type_info.json \
+--type_info_path=type_info_panNuke.json \
 --batch_size=32 \
 --model_mode=fast \
---model_path=models/pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--model_path=/home/robj/Projects/hover_net/lowerGU/imageNetPreTrained_2/01/net_epoch=50.tar \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 wsi \
---input_dir=/data/UHCW/WSIs/Test/ \
---output_dir=/data/Test/hoverNetUHCWTest/ \
+--input_dir=/data/UHCW/WSIs/UHCW_Bladder \
+--output_dir=/data/UHCW/LowerGU_HoVer-Net_annotations/ \
 --input_mask_dir= \
 --save_thumb \
 --save_mask
